28e8ba2964c5cfb72474b90a3ce18aeca7c66ee1
dinesh parmar <shashikha1000@gmail.com>
2021-09-16 21:23:06 +0530

    details page going

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -23,9 +23,9 @@ class DetailsPage extends StatelessWidget {
                   ),
                   SizedBox(
                     height: 16.h,),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 46.w,),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 30.w,),
                 child:Container(
-                  height: 154.h,
+                  height: 188.h,
                 child:  Card(
                   color: Color(0xffB2B9F1),
                     shape: RoundedRectangleBorder(
@@ -42,12 +42,20 @@ class DetailsPage extends StatelessWidget {
                               Text("Title",style: TextStyle(fontSize: 25.sp),),
                               ),
                               Positioned(
+                                top: 3.23,
+                                left: 264.72,
+                                child:
+                              IconButton(onPressed: (){
+                                
+                              }, icon:Icon(Icons.arrow_forward)),
+                              ),
+                              Positioned(
                                 left: 38.0,
                                 top: 53.0,
                                 child:Wrap(
                                   children: [
                                     Text("Location"),
-                                    Text("                        Date Time"),
+                                    Text("                               Date Time"),
                                   ],
                                 )
 
@@ -55,17 +63,44 @@ class DetailsPage extends StatelessWidget {
                               Positioned(
                                 left: 28.0,
                                 top: 81.0,
-                                child:
-                                Text("In Progress"),
-                              ),
-                          RaisedButton(onPressed: (){
-                            
-                          },
-                          child: Text("Complete"),),
-                          RaisedButton(onPressed: (){
+                                child:Wrap(
+                                  children: [
+                                    Text("In Progress"),
+                                    Text("                            Rs 200/250")
 
-                          },
-                            child: Text("Complete"),)
+                                  ],
+                                )
+                              ),
+                          Positioned(
+                            top: 119.0,
+                              left: 7.0,
+
+                              child: Row(
+                            children: [
+                              RaisedButton(
+                                onPressed: (){
+                              },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.r)
+                            ),
+                                child: Text("Complete"),),
+                              Padding(padding: EdgeInsets.symmetric(horizontal: 6.w)),
+                              RaisedButton(onPressed: (){
+                              },
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.r)
+                                ),
+                                child: Text("Hold on"),),
+                              Padding(padding: EdgeInsets.symmetric(horizontal: 6.w)),
+                              RaisedButton(onPressed: (){
+                              },
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.r)
+                                ),
+                                child: Text("Reject"),)
+                            ],
+                          ))
+                         
                             ],
 
 
@@ -77,14 +112,61 @@ class DetailsPage extends StatelessWidget {
 
                       ),
 
-                      trailing: Icon(Icons.arrow_forward),
+                      
+
+
 
                     ),
                     
 
+                  ),
+
+                    
+                  ),
+
+                  ),
+                  Text("Remarks",style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 27.sp,fontWeight: FontWeight.w500)),),
+                  SizedBox(height: 8.h),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child:  Container(
+                height: 156.h,
+                child:  Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.r)
+                    ),
+                    color: Color(0xffB2B9F1),
+                 child: TextField(
+
+                    decoration: InputDecoration(
+                      border: InputBorder.none
+
+                    ),
+
+
+
+
+
+                  ),
+
+
+
+
+                  ),
+
+
+                  ),
+
+                  ),
+                  Column(
+                    children: [
+                      ListTile(
+                        title: Text("Date Time"),
+                        subtitle: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies nisl hac adipiscing consequat, urna di g"),
+                      )
+                    ],
                   )
-                  ),
-                  ),
+
+                  
                   ],
 
               ),
